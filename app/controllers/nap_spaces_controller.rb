9635f2738d35425a437a5f12e6b4a95971bdf8d7
Thomas Cochrane <thomasrcochrane@icloud.com>
2022-11-29 22:16:38 +1100

    authentication skip fixed

--- app/controllers/nap_spaces_controller.rb
+++ app/controllers/nap_spaces_controller.rb
@@ -1,5 +1,5 @@
 class NapSpacesController < ApplicationController
-  skip_before_action :authenticate_user!, only: [:home, :new, :create]
+  skip_before_action :authenticate_user!, only: :home
   def home
   end
 
