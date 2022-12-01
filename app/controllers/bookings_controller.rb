b2f194befc5268ef83c5b50146d9dd00648fcdaf
Thomas Cochrane <thomasrcochrane@icloud.com>
2022-12-01 20:15:08 +1100

    bookings can be confirmed/declined by host, and cancelled by user (back end only, no views implemented)

--- app/controllers/bookings_controller.rb
+++ app/controllers/bookings_controller.rb
@@ -1,2 +1,19 @@
 class BookingsController < ApplicationController
+  def confirm
+    @booking.confirm if current_user == @booking.nap_space.user
+  end
+
+  def decline
+    @booking.decline if current_user == @booking.nap_space.user
+  end
+
+  def cancel
+    @booking.cancel if current_user == @booking.user
+  end
+
+  private
+
+  def set_booking
+    @booking = Booking.find(params[:id])
+  end
 end
