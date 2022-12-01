91c48d33d6ef0c0080fa6d95ddd03cc4b25b0533
Claire Fernandes <claire@hotdoc.com.au>
2022-12-01 22:07:29 +1100

    added functionality for user to make a booking. imported stylesheets to application.scss

--- app/controllers/bookings_controller.rb
+++ app/controllers/bookings_controller.rb
@@ -1,2 +1,27 @@
 class BookingsController < ApplicationController
+
+  def new
+    @nap_space = NapSpace.find(params[:nap_space_id])
+    @booking = Booking.new
+  end
+
+  def create
+    @user = current_user
+    @nap_space = NapSpace.find(params[:nap_space_id])
+    @booking = Booking.new(strong_params)
+    @booking.nap_space = @nap_space
+    @booking.user = @user
+    @booking.confirmation_status = 'requested'
+    if @booking.save
+      redirect_to bookings_path, status: :see_other
+    else
+      render :new, status: :unprocessable_entity
+    end
+  end
+
+  private
+
+  def strong_params
+    params.require(:booking).permit(:start_time, :end_time)
+  end
 end
