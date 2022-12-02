21f2f68571628bb9ec98863fae9ff484f0560093
Thomas Cochrane <thomasrcochrane@icloud.com>
2022-12-02 20:39:42 +1100

    booking confirm/decline/cancel implemented with basic front end

--- app/controllers/bookings_controller.rb
+++ app/controllers/bookings_controller.rb
@@ -1,27 +1,45 @@
 class BookingsController < ApplicationController
-  before_action :set_booking, only: %i[confirm decline cancel]
+  before_action :set_booking, only: %i[show confirm decline cancel host?]
 
   def index
     @bookings = Booking.where(user_id: current_user.id)
   end
 
   def show
-    @nap_space = @booking.nap_space
   end
 
   def confirm
-    @booking.confirm if current_user == @booking.nap_space.user
+    return unless user_is_host?
+    return if cancelled?(@booking)
+
+    @booking.confirmation_status = 'confirmed'
+    redirect_to booking_path(@booking) if @booking.save!
   end
 
   def decline
-    @booking.decline if current_user == @booking.nap_space.user
+    return unless user_is_host?
+    return if cancelled?(@booking)
+
+    @booking.confirmation_status = 'declined'
+    redirect_to booking_path(@booking) if @booking.save!
   end
 
   def cancel
-    @booking.cancel if current_user == @booking.user
+    return unless current_user == @booking.user
+
+    @booking.confirmation_status = 'cancelled'
+    redirect_to booking_path(@booking) if @booking.save
   end
 
   private
+
+  def cancelled?(booking)
+    booking.confirmation_status == 'cancelled'
+  end
+
+  def user_is_host?
+    current_user == @booking.nap_space.user
+  end
 
   def set_booking
     @booking = Booking.find(params[:id])
