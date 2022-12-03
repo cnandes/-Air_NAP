a548ad8f747f8911d9ffc80eebcc6198fe2cd187
gurmukhsingh <gurmukhs300@gmail.com>
2022-12-03 15:23:02 +1100

    seed-edit

--- db/seeds.rb
+++ db/seeds.rb
@@ -6,7 +6,6 @@
 #   movies = Movie.create([{ name: "Star Wars" }, { name: "Lord of the Rings" }])
 #   Character.create(name: "Luke", movie: movies.first)
 puts "cleaning database"
-NapSpace.destroy_all
 
 puts "creating nap_spaces"
 
@@ -16,7 +15,7 @@ puts "creating nap_spaces"
     description: Faker::Company.catch_phrase,
     address: Faker::Address.full_address,
     cost_per_hr: Faker::Number.decimal(l_digits: 2),
-    image_url: Faker::LoremFlickr.image(search_terms: ["sofa", "bed"], match_all: true),
+    image_url: Faker::LoremFlickr.image(search_terms: ["sofa", "bed"], match_all: true)
   )
   puts "napspace with id: #{nap_space.id} has been created"
 end
