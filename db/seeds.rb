b16b48085643c20a16374bdb6a6a3ff4e57eeed9
Archana Majumdar <archie.majumdar@gmail.com>
2022-12-03 12:11:25 +1100

    napspace seeds

--- db/seeds.rb
+++ db/seeds.rb
@@ -5,3 +5,20 @@
 #
 #   movies = Movie.create([{ name: "Star Wars" }, { name: "Lord of the Rings" }])
 #   Character.create(name: "Luke", movie: movies.first)
+puts "cleaning database"
+NapSpace.destroy_all
+
+puts "creating nap_spaces"
+
+10.times do
+  nap_space = NapSpace.create(
+    user_id: rand(0..3),
+    description: Faker::Company.catch_phrase,
+    address: Faker::Address.full_address,
+    cost_per_hr: Faker::Number.decimal(l_digits: 2),
+    image_url: Faker::LoremFlickr.image(search_terms: ["bed"], match_all: true),
+  )
+  puts "napspace with id: #{nap_space.id} has been created"
+end
+
+puts "finished"
