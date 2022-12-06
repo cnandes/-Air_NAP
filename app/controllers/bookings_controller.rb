b698893f7aa3fc9367e39b6ad015a8230b63fe42
Thomas Cochrane <thomasrcochrane@icloud.com>
2022-12-06 19:12:16 +1100

    bookings are now ordered by start time

--- app/controllers/bookings_controller.rb
+++ app/controllers/bookings_controller.rb
@@ -2,7 +2,7 @@ class BookingsController < ApplicationController
   before_action :set_booking, only: %i[show confirm decline cancel host?]
 
   def index
-    @bookings = Booking.where(user_id: current_user.id)
+    @bookings = Booking.where(user_id: current_user.id).order(:start_time)
   end
 
   def new
