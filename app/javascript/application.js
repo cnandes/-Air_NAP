2614b9f3ccded98677dd6903bc7c366f92046057
Thomas Cochrane <thomasrcochrane@icloud.com>
2023-02-18 17:17:50 +1100

    refactor

--- app/javascript/application.js
+++ app/javascript/application.js
@@ -2,6 +2,3 @@
 import "@hotwired/turbo-rails"
 import "./controllers"
 import "bootstrap"
-
-const toastElList = document.querySelectorAll('.toast')
-const toastList = [...toastElList].map(toastEl => new bootstrap.Toast(toastEl, option))
